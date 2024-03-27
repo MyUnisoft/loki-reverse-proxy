9465cc837b13494eedd7706643a22e61069fe41d
fraxken <gentilhomme.thomas@gmail.com>
2024-03-27 21:40:07 +0100

    fix: coerce boolean value

--- src/modules/env.ts
+++ src/modules/env.ts
@@ -7,10 +7,10 @@ const envSchema = z.object({
   LOKI_URL: z.string().url().trim(),
   TOKEN_CACHE_MS: z.coerce.number().optional().default(1_000 * 60 * 5),
   SERVER_PORT: z.coerce.number().optional().default(0),
-  SERVER_SSL_ENABLED: z.boolean().optional().default(false),
+  SERVER_SSL_ENABLED: z.coerce.boolean().optional().default(false),
   SERVER_SSL_CERT: z.string().optional(),
   SERVER_SSL_KEY: z.string().optional(),
-  TRUST_PROXY: z.boolean().optional().default(false)
+  TRUST_PROXY: z.coerce.boolean().optional().default(false)
 });
 
 export type ENV = z.infer<typeof envSchema>;
